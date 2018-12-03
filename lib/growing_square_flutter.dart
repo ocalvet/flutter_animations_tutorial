78d715c17210284dd61822dd6345ca77001deab5
Ovidio R. Calvet <calveto@gmail.com>
2018-12-02 20:16:52 -0500

    Playing around

--- lib/growing_square_flutter.dart
+++ lib/growing_square_flutter.dart
@@ -11,31 +11,55 @@ class _StateFlutterAnimationsTester extends State<FlutterAnimationsTester>
     with TickerProviderStateMixin {
   AnimationController _animationController;
   Animation<double> _mainAnimation;
+  Animation<double> _opacityAnimation;
   Animation<Offset> _slideAnmation;
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(
-        vsync: this, duration: Duration(milliseconds: 1500));
-    _mainAnimation = CurvedAnimation(
-      parent: _animationController,
-      curve: Interval(0.0, 0.5, curve: Curves.easeInOut),
-    );
-    _slideAnmation = Tween(
-      begin: Offset(0, 0),
-      end: Offset(0, 1),
-    ).animate(_mainAnimation);
-    _animationController.forward();
+    this.setState(() {
+      _animationController = AnimationController(
+          vsync: this, duration: Duration(milliseconds: 3000));
+      _mainAnimation = CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(0.0, 1.0, curve: Curves.easeInOut),
+      );
+      _opacityAnimation = Tween(begin: 0.0, end: 1.0).animate(
+        CurvedAnimation(
+          parent: _animationController,
+          curve: Interval(
+            0.0,
+            1.0,
+            curve: Curves.ease,
+          ),
+        ),
+      );
+      _slideAnmation = Tween(
+        begin: Offset(0, 0),
+        end: Offset(3, 1),
+      ).animate(_mainAnimation);
+      _animationController.forward();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    print(
+        'rendering ${(_opacityAnimation != null ? _opacityAnimation?.value : 0).toString()}');
     return SlideTransition(
       position: _slideAnmation,
-      child: Container(
-        width: 100,
-        height: 100,
-        color: Colors.red,
+      child: GestureDetector(
+        child: Opacity(
+          child: Container(
+            width: 100,
+            height: 100,
+            color: Colors.red,
+          ),
+          opacity: 1,
+        ),
+        onTap: () {
+          _animationController.reset();
+          _animationController.forward();
+        },
       ),
     );
   }
