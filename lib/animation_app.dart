321c5cef9a361a219d6f96306d6b577c92dbb84e
Ovidio R. Calvet <calveto@gmail.com>
2018-12-04 23:06:42 -0500

    Star stagger animation

--- lib/animation_app.dart
+++ lib/animation_app.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animations_tutorial/growing_square.dart';
 import 'package:flutter_animations_tutorial/growing_square_flutter.dart';
+import 'package:flutter_animations_tutorial/staggered_animations.dart';
 
 class AnimationApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: FlutterAnimationsTester(),
-    );
+    // return Center(
+    //   child: FlutterAnimationsTester(),
+    // );
+    return MaterialApp(
+      
+      home: StaggerAnimationWidget()
+      );
   }
 }
