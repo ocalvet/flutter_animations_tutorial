24905dc4b981daff2962f35ab0e124990fcac809
Ovidio R. Calvet <calveto@gmail.com>
2019-02-12 17:48:08 -0500

    Use the text in the widget

--- lib/animation_app.dart
+++ lib/animation_app.dart
@@ -4,6 +4,7 @@ import 'package:flutter_animations_tutorial/rotating_text/rotating_square.dart';
 class AnimationApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    String sampleText = 'TEST';
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Stack(
@@ -11,7 +12,9 @@ class AnimationApp extends StatelessWidget {
           Positioned(
             left: 120,
             top: 150,
-            child: RotatingSquare(),
+            child: RotatingSquare(
+              displayText: sampleText,
+            ),
           )
         ],
       ),
